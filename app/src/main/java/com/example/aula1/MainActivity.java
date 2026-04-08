27e0c1223981579b37193a16f24bb10292a15feb
VerttB <alyssonoliveira456@gmail.com>
2026-04-08 10:43:24 -0300

    inicio

--- app/src/main/java/com/example/aula1/MainActivity.java
+++ app/src/main/java/com/example/aula1/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.aula1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -18,10 +24,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linearlayout);
 //        setContentView(R.layout.linearlayout);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button myButton = findViewById(R.id.edit_btn);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG", "Botao Clicado");
+                EditText edit = findViewById(R.id.edit_text_id);
+                edit.setText(R.string.alysson);
+                Toast.makeText(MainActivity.this, R.string.alysson, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
 
     }
 
@@ -53,3 +75,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
